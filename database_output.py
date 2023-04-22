a17df5060ca077bba96e90ba651a4c7c4b58f0f6
Griffin <griffincolomer@gmail.com>
2023-04-22 18:09:23 -0500

    Fixed the output so now the database willl stretch to the correct length

--- database_output.py
+++ database_output.py
@@ -11,27 +11,21 @@ class Database_Output(tk.Frame):
     def create_widgets(self, fields):
         # create labels for each field in the tuple
         for i, field in enumerate(fields):
-            tk.Label(self, text=field, relief=tk.RIDGE, width=12, font='Helvetica 10 bold').grid(row=0, column=i)
+            tk.Label(self, text=field, relief=tk.RIDGE, width=12, font='Helvetica 10 bold').grid(row=0, column=i, sticky=tk.NSEW)
         
         # create a separator line
-        tk.Frame(self, height=2, bd=1, relief=tk.SUNKEN).grid(row=1, columnspan=len(fields))
+        tk.Frame(self, height=2, bd=1, relief=tk.SUNKEN).grid(row=1, columnspan=len(fields), sticky=tk.NSEW)
     
     def update(self, data):
         # clear any previous rows
         for widget in self.winfo_children():
-            if isinstance(widget, tk.Label) and widget.grid_info()['row'] > 0:
+            if isinstance(widget, tk.Label) and widget.grid_info()['row'] > 1:
                 widget.grid_forget()
         
         # output each tuple as a row of labels
         for i, row in enumerate(data):
             for j, item in enumerate(row):
-                tk.Label(self, text=item, relief=tk.RIDGE, width=12, font='Helvetica 10').grid(row=i+2, column=j)
-    
-    def clear(self):
-        # clear all rows
-        for widget in self.winfo_children():
-            if isinstance(widget, tk.Label) and widget.grid_info()['row'] > 0:
-                widget.grid_forget()
+                tk.Label(self, text=item, relief=tk.RIDGE, font='Helvetica 10').grid(row=i+2, column=j, sticky=tk.NSEW)
 
 def getFields(columns):
     fields = []
