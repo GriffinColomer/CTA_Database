2e4675c6ae907cde89c8629ccdb8e7752b243fbe
Griffin <griffincolomer@gmail.com>
2023-04-22 13:42:05 -0500

    added comments and set up basic database viewing

---
+++ gui.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+class DatabaseOutput(tk.Frame):
+    def __init__(self, master=None, data=None, fields=None):
+        super().__init__(master)
+        self.grid()
+        self.create_widgets(fields)
+        if data:
+            self.update(data)
+
+    def create_widgets(self, fields):
+        # create labels for each field in the tuple
+        for i, field in enumerate(fields):
+            tk.Label(self, text=field, relief=tk.RIDGE, width=12, font='Helvetica 10 bold').grid(row=0, column=i)
+        
+        # create a separator line
+        tk.Frame(self, height=2, bd=1, relief=tk.SUNKEN).grid(row=1, columnspan=len(fields))
+    
+    def update(self, data):
+        # clear any previous rows
+        for widget in self.winfo_children():
+            if isinstance(widget, tk.Label) and widget.grid_info()['row'] > 0:
+                widget.grid_forget()
+        
+        # output each tuple as a row of labels
+        for i, row in enumerate(data):
+            for j, item in enumerate(row):
+                tk.Label(self, text=item, relief=tk.RIDGE, width=12, font='Helvetica 10').grid(row=i+2, column=j)
+    
+    def clear(self):
+        # clear all rows
+        for widget in self.winfo_children():
+            if isinstance(widget, tk.Label) and widget.grid_info()['row'] > 0:
+                widget.grid_forget()
+
+def getFields(columns):
+    fields = []
+    for x in columns:
+        fields.append(x[0])
+    return tuple(fields)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import mysql.connector
-
+import gui
 
 if __name__ == "__main__":
+  # sets up the window for the output
+  root = gui.tk.Tk()
+  # connects to the database
   mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
@@ -9,9 +12,17 @@ if __name__ == "__main__":
   database="university"
   )
 
+  # goes into database executes command and gets the attribute names(colums) as well as the data(result)
   mycursor = mydb.cursor()
-
   mycursor.execute("select RegID, CourseName, ProfFname, ProfLname, StdFname, StdLname from registration, courses, professors, student where professors.ProfID = courses.ProfID and courses.courseID = registration.CourseID and student.StdNo = registration.StdNo")
+  columns = mycursor.description 
+  result = mycursor.fetchall()
 
-  for x in mycursor:
-    print(x)
+  # outputs the data into a the database output class to make the database screen
+  output1 = gui.DatabaseOutput(root, fields = gui.getFields(columns))
+  output1.pack()
+  output1.update(result)
+
+  # Runs the Gui
+  root.mainloop()
+  
